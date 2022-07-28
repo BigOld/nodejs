c840670ed9473d1b42a2873517135e584ec9dba1
Rongrubujing <705456001@qq.com>
2022-07-28 10:47:18 +0800

    feat: RESTFULL APi

--- Express/email-template/server/index.js
+++ Express/email-template/server/index.js
@@ -1,12 +1,21 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
+const api = require('./middleware/api')
 const app = express()
-const templateRouter = require('./routes/template')
+const templateRouter = require('./routes/')
 
 mongoose.connect('mongodb://127.0.0.1:27017/template', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use(api)
 
 app.use('/xhr/v1', templateRouter)
 
